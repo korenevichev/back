96ec842f16164e13d2cb7cd5af73f3a3e5d7e334
Lisa K <korenevich.elizaveta@gmail.com>
2020-01-17 17:06:45 +0300

    draft
    awesome work with front
    
    awesome work
    
    add updates
    
    update
    
    integrate with gena
    
    add missing files
    
    add seed.rb
    
    add error
    
    update seed file

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -5,80 +5,23 @@ require 'json'
 require 'mock/mock_stream_service'
 
 class StreamsController < ApplicationController
-  PER_PAGE = 10
+  include ActionController::Live
 
-  def index
-    stream_url = 'rtsp://admin:1qaz!QAZ@192.168.112.42:554/ch1/main'
+  def identify
+    response.headers['Content-Type'] = 'text/event-stream'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    stream_url = 'rtsp://admin:1qaz!QAZ@192.168.112.70:554/ch1/main'
     stream_analyzer_service = StreamAnalyzerService.new(stream_url)
-    image_analyzer_service = ImageAnalyzerService.new
-
-    videoCapture = FaceSDK::VideoCapture.new(stream_url)
-
-    blob = videoCapture.read_frame
-
-
-    # frames = image_analyzer_service.frames(blob)
-    # fr1 = image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/6.jpg')
-    # fr2 = image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/7.jpg')
-    # d1 = fr1.first['Descriptor']
-    # d2 = fr2.first['Descriptor']
-    # binding.pry
-    #FaceSDK::CompareFacesByDescriptor(d1, d2, FaceSDK::GetDefaultComparisonParameters())
-    # descriptor1 = Descriptor.first.descriptor_values
-    # descriptor2 = Descriptor.last.descriptor_values
-    # FaceSDK::CompareFacesByDescriptor(descriptor2, descriptor2, FaceSDK::GetDefaultComparisonParameters())
     descriptors = Descriptor.all
-    t = Thread.new do
-      loop do
-        stream_analyzer_service.start_frames_analyzing(descriptors)
-        sleep 1
-      end
-    end
-
-    trap("INT") do
-      t.exit
-      exit
-    end
 
     stream_analyzer_service.store_frames
 
+    response_frame = stream_analyzer_service.start_frames_analyzing(descriptors)
+    i = Base64.encode64(response_frame[:image])
 
-    # fr1 = image_analyzer_service.frames('/Users/lizavetakarenevich/Downloads/4.png')
-    # fr2 = image_analyzer_service.frames('/Users/lizavetakarenevich/Downloads/5.png')
-    #image_analyzer_service.add_employee('/Users/lizavetakarenevich/Downloads/4.png', 'Oleg', 'Gubin', 'Head of Rnd')
-    #image_analyzer_service.add_employee('/Users/lizavetakarenevich/Downloads/5.png', 'Ksenia', 'Meshkova', 'Rnd Engineer')
-    #image_analyzer_service.add_employee('/Users/lizavetakarenevich/Downloads/6.jpg', 'Angelina', 'Jolie', 'Actress')
-
-
-    # EM.run {
-    #   EM::WebSocket.run(:host => "127.0.0.1", :port => 8080) do |ws|
-    #     ws.onopen { |handshake|
-    #       files = ['/Users/lizavetakarenevich/Downloads/1.jpeg', '/Users/lizavetakarenevich/Downloads/2.jpg']
-    #       files.each do |file|
-    #         f = File.binread(file).to_s
-    #         data = {employees: [], image: f}
-    #         ws.send data.to_json
-    #       end
-    #       puts "WebSocket connection open"
-    #
-    #       # Access properties on the EM::WebSocket::Handshake object, e.g.
-    #       # path, query_string, origin, headers
-    #
-    #       # Publish message to the client
-    #
-    #       # ws.send "Hello Client, you connected to #{handshake.path}"
-    #     }
-    #
-    #     ws.onclose { puts "Connection closed" }
-    #
-    #     ws.onmessage { |msg|
-    #       puts "Recieved message: #{msg}"
-    #       ws.send "Pong: #{msg}"
-    #     }
-    #   end
-    # }
-  end
-
-  def identify;
+    sse = SSE.new(response.stream, retry: 300)
+    sse.write({ employees: response_frame[:employees], image: i })
+  ensure
+    sse.close if sse
   end
 end
--- app/services/stream_analyzer_service.rb
+++ app/services/stream_analyzer_service.rb
@@ -13,7 +13,6 @@ class StreamAnalyzerService
     @frame_storage_service = FrameStorageService.new
     @image_analyzer_service = ImageAnalyzerService.new
     @compare_params = FaceSDK::default_comparison_params
-    @n = 0
   end
 
   def read_frame
@@ -27,30 +26,29 @@ class StreamAnalyzerService
   end
 
   def store_frames
-    while video_stream_running?
-      puts 'thread 2'
-      frame_storage_service.store(read_frame)
-      sleep 1
-    end
+    frame = read_frame
+    raise StandardError, 'Empty frame' unless frame.present?
+    frame_storage_service.store(read_frame)
   end
 
   def start_frames_analyzing(descriptors)
-    @n = @n + 1
-    puts 'thread 1'
     next_frame = frame_storage_service.next_frame
-    #if next_frame.present?
-      #frames = @image_analyzer_service.frames(blob: next_frame)
-       frames = @image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/5.png')
-       frames = @image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/6.jpg') if @n%2 == 0
+    found_faces = []
+    # n = rand(8)
+    if next_frame.present?
+      # frane = File.binread('/Users/lizavetakarenevich/Downloads/5.png')
+      # frane = File.binread('/Users/lizavetakarenevich/Downloads/6.jpg') if n % 2 == 0
+      frames = @image_analyzer_service.frames(blob: next_frame)
+      # frames = @image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/5.png')
+      # frames = @image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/6.jpg') if n % 2 == 0
       frames.each do |frame|
         descriptors.each do |descriptor|
-          person_is_found = FaceSDK::compare_faces_by_descriptor(frame['Descriptor'], descriptor.descriptor_values)
-            puts 'comparison ' + person_is_found.to_s
-            puts 'face is found'  if person_is_found > @compare_params[:threshold99]
-            puts 'face not found' if person_is_found < @compare_params[:threshold99]
+          comparison_result = FaceSDK::compare_faces_by_descriptor(frame['Descriptor'], descriptor.descriptor_values)
+          person_is_found = comparison_result > @compare_params[:threshold99]
+          found_faces << descriptor.person if person_is_found
         end
       end
-    #end
-
+    end
+    { image: next_frame, employees: found_faces }
   end
 end
