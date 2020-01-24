2ebe79ea1bc740a8273c868a224b21b500a52ce9
Lisa K <korenevich.elizaveta@gmail.com>
2020-01-24 16:18:33 +0300

    Add using c++ locally. Add dockerfile

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,5 @@
 #
 # Model for accommodations table
 class Person < ApplicationRecord
-  has_one :descriptor
+  has_one :descriptor, dependent: :destroy, foreign_key: :person_id
 end
--- app/services/stream_analyzer_service.rb
+++ app/services/stream_analyzer_service.rb
@@ -1,54 +1,40 @@
 # frozen_string_literal: true
 
-require 'mock/mock_stream_service'
+require '/files/libfacesdk_ruby'
 require 'frame_storage_service'
+require 'image_analyzer_service'
 
 class StreamAnalyzerService
-  include FaceSDK
   attr_reader :frame_storage_service
 
-  def initialize(stream_url)
-    @stream_url = stream_url
-    @video_capture = VideoCapture.new(@stream_url)
+  CONFIGURATION = {
+    FaceDetectionModelsFolder: '/facesdk/graphs/',
+    FaceRecognitionModelsFolder: '/facesdk/graphs/',
+    UseGPU: true,
+    LogFilePath: '',
+    ResetCount: 0
+  }
+
+  def initialize
     @frame_storage_service = FrameStorageService.new
-    @image_analyzer_service = ImageAnalyzerService.new
-    @compare_params = FaceSDK::default_comparison_params
-  end
-
-  def read_frame
-    return unless @video_capture.opened
-
-    @video_capture.read_frame
-  end
-
-  def video_stream_running?
-    @video_capture.opened
-  end
-
-  def store_frames
-    frame = read_frame
-    raise StandardError, 'Empty frame' unless frame.present?
-    frame_storage_service.store(read_frame)
+    @image_analyzer_service = ImageAnalyzerService.new(CONFIGURATION)
+    @compare_params = FaceSDK::GetDefaultComparisonParameters()
   end
 
   def start_frames_analyzing(descriptors)
-    next_frame = frame_storage_service.next_frame
+    next_frame = frame_storage_service.fetch.first
     found_faces = []
-    # n = rand(8)
-    if next_frame.present?
-      # frane = File.binread('/Users/lizavetakarenevich/Downloads/5.png')
-      # frane = File.binread('/Users/lizavetakarenevich/Downloads/6.jpg') if n % 2 == 0
+    if next_frame
       frames = @image_analyzer_service.frames(blob: next_frame)
-      # frames = @image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/5.png')
-      # frames = @image_analyzer_service.frames(path: '/Users/lizavetakarenevich/Downloads/6.jpg') if n % 2 == 0
       frames.each do |frame|
         descriptors.each do |descriptor|
-          comparison_result = FaceSDK::compare_faces_by_descriptor(frame['Descriptor'], descriptor.descriptor_values)
+          comparison_result = FaceSDK::CompareFacesByDescriptor(frame['Descriptor'], descriptor.descriptor_values)
           person_is_found = comparison_result > @compare_params[:threshold99]
           found_faces << descriptor.person if person_is_found
         end
       end
     end
+    puts ">>>>111 #{found_faces.first.name}" if found_faces.size > 0
     { image: next_frame, employees: found_faces }
   end
 end
